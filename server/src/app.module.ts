b8daf8cc382ed1620288e0f4c14f8203bd476c57
Ilya <i.shyshparonak@gmail.com>
2024-03-05 16:57:51 +0300

    feat: initialize config module globally

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import securityConfig from './config/security.config';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '../.env',
+      load: [securityConfig],
+      isGlobal: true,
+    }),
+  ],
 })
 export class AppModule {}
