606df08a9738d0bc6571f4242c44948da472334d
Ilya <i.shyshparonak@gmail.com>
2024-03-05 19:56:20 +0300

    feat: create error message enum

---
+++ server/src/enums/error-messages.enum.ts
@@ -0,0 +1,6 @@
+export enum ErrorMessage {
+  UserAlreadyExists = 'User already exists',
+  PasswordsDontMatch = 'Passwords dont match',
+  UserCreationFailed = 'User creation failed',
+  UserDoesNotExist = 'User does not exist',
+}
