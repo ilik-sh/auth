35be128b8076c17d426823f2c1b0377178422dcd
Ilya <i.shyshparonak@gmail.com>
2024-03-05 19:59:20 +0300

    fix: fix security logic

--- server/src/libs/security/strategies/access-token.strategy.ts
+++ server/src/libs/security/strategies/access-token.strategy.ts
@@ -2,16 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
-import { UserRepository } from 'repositories/user.repository';
-import { User } from '@prisma/client';
 import { UserSessionDto } from 'dtos/user-session.dto';
 
 @Injectable()
 export class AccessTokenStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor(
-    private readonly config: ConfigService,
-    private readonly userRepository: UserRepository,
-  ) {
+  constructor(private readonly config: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
