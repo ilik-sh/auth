c4e2de7e8b3d607f829da66ace56f30b7a81e20b
Ilya <i.shyshparonak@gmail.com>
2024-03-05 17:07:42 +0300

    feat: create security service and module

---
+++ server/src/libs/security/security.service.ts
@@ -0,0 +1,43 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '@prisma/client';
+import { UserSessionDto } from 'dtos/user-session.dto';
+import * as crypto from 'crypto';
+
+@Injectable()
+export class SecurityService {
+  constructor(
+    private readonly config: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async hashPassword(password: string) {
+    const hash = crypto.createHash('MD5');
+    return hash.update(password).digest('hex');
+  }
+
+  async comparePasswords(plainPassword: string, hashedPassword: string) {
+    return (await this.hashPassword(plainPassword)) === hashedPassword;
+  }
+
+  generateTokens(model: User) {
+    const payload = UserSessionDto.toPlainObject(model);
+    const securityConfig = this.config.get('security');
+
+    const access = securityConfig.access;
+    const refresh = securityConfig.refresh;
+
+    const accessToken = this.jwtService.sign(payload, {
+      secret: access.secret,
+      expiresIn: access.expiry,
+    });
+
+    const refreshToken = this.jwtService.sign(payload, {
+      secret: refresh.secret,
+      expiresIn: refresh.expiry,
+    });
+
+    return { accessToken, refreshToken };
+  }
+}
